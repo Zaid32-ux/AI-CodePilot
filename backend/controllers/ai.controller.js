ef91452dbdb3f8e0e375be5654d46c25258204c2
Zaid32-ux <Zgour5071@gmail.com>
2026-06-18 18:53:45 +0530

    add AI code review generation and save reviews to MongoDB

---
+++ backend/controllers/ai.controller.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
+import { Prompt } from "../Model/promptSchema.js";
+import aiService from "../services/ai.service.js";
+
+// ---------------- GENERATE REVIEW ----------------
+export const getReview = catchAsyncErrors(async (req, res) => {
+  const { code } = req.body;
+
+  if (!code) {
+    return res.status(400).json({
+      success: false,
+      message: "Code is required!",
+    });
+  }
+
+  const review = await aiService(code);
+
+  const prompt = await Prompt.create({
+    code,
+    review,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Review generated successfully!",
+    prompt,
+  });
+});
