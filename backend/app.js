925ff7394b1f893dc09f7e7e56615f5e6d0a9627
Zaid32-ux <Zgour5071@gmail.com>
2026-07-11 11:14:02 +0530

    Adding cors cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import  dbConnection  from "./database/dbConnection.js";
 import authRoutes from "./routes/auth.routes.js";
-import aiRoutes from "./routes/auth.routes.js"
+import aiRoutes from "./routes/ai.routes.js"
 
 config({ path: "./.env" });
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
